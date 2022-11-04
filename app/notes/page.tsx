0c63e70f3498b7a22aa0c0ffd14585d0ff9540a1
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2022-11-03 21:13:42 -0400

    First Commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,12 @@
+
+/**
+ * Default server
+ */
+
+export default async function NotesPage() {
+    return (
+        <div>
+            <h1>Notes</h1>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <>
+            <h1>ShellHacks 2023</h1>
+            <p>Welcome to ShellHacks 2023</p>
+        </>
+    )
+}
+
+export default HomePage
